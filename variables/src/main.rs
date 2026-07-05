0d6e8aca4ef88c905689ae93fa4ce64094a15682
watto <andreiignatovskii@MacBook-Pro-watto.local>
2026-07-05 19:03:02 +0300

    new program and its code

---
+++ variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut x = 5;
+    println!("Значение в x: {x}");
+    x = 6;
+    println!("Значение в x: {x}");
+}
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/andreiignatovskii/learning_rust/variables/target/debug/variables: /Users/andreiignatovskii/learning_rust/variables/src/main.rs
